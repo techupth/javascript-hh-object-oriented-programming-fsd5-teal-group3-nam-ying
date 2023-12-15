af20cb99d65e7ae46e6443c3aa62d8b4dfc07bd0
suphattraying <suphattra.spk@gmail.com>
2023-12-15 12:59:14 +0700

    finished

--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,49 @@
-class Notification { }
+class Notification {
+  constructor(notificationId, createdTime, content) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    
+  }
+  sendDetail() {
+    return `${this.notificationId} ${this.createdTime}(${this.content})`;
+  }
+}
 
-class EmailNotification { }
+class EmailNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content);
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to  ${this.receiver}`);
+  }
+}
 
-class SMSNotification { }
+class SMSNotification extends Notification {
+  constructor(notificationId, createdTime, content, phoneNumber) {
+    super(notificationId, createdTime, content);
+    this.phoneNumber = phoneNumber;
+  }
+  send() {
+    console.log(`Notification has been sent to  ${this.phoneNumber}`);
+  }
+}
+
+const emil = new EmailNotification(
+  "001",
+  new Date(),
+  "This is email noti message.",
+  "knot@gmail.com"
+);
+console.log(emil.sendDetail());
+emil.send();
+
+const phone = new SMSNotification(
+  "001",
+  new Date(),
+  "This is sms noti message",
+  "0923232332"
+);
+console.log(phone.sendDetail());
+phone.send();
--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,89 @@
 //  Start coding here
+class User{
+    constructor(id,name, email){
+        this.id = id;
+        this.name=name
+        this.email=email
+}
+}
+
+class PostList{
+    constructor(posts,addPost,sharePost){
+        this.posts= posts;
+        this.addPost=addPost
+        this.sharePost= sharePost
+    }
+    addNewPost(){
+        console.log(`You've shared post ${this.posts}to your friend.`)
+}
+}
+
+let user11 = new PostList("hi sunny day ","john@gmail.com","share")
+console.log(user11)
+user11.addNewPost()
+
+class Post{
+    constructor(id,title,content,comment){
+        this.id=id
+        this.title=title
+        this.content=content
+        this.comment=[comment]
+    }
+    addComment(comment) {
+        this.comment.push(comment);
+      }
+}
+let mypost = new Post('1','My first blog', 'This is my first blog')
+mypost.addComment('Great job!');
+console.log(mypost)
+
+class Comment {
+    constructor(id, content, createdBy) {
+      this.id = id;
+      this.content = content;
+      this.createdBy = createdBy;
+      this.likes = 0;
+    }
+  
+    addLike() {
+      this.likes += 1;
+    }
+  }
+  class Facebook {
+    constructor() {
+      this.groupList = [groupList];
+      this.pageList = [pageList];
+    }
+  
+    addGroup(groupList) {
+      this.groupList.push(groupList);
+    }
+  
+    addPage(pageList) {
+      this.pageList.push(pageList);
+    }
+  }
+  
+  class FacebookPage {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  class FacebookGroup {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  class Notification {
+    constructor(id) {
+      this.id = id;
+    }
+    send(commentCreatedBy, postTitle) {
+        console.log(`Notification: ${commentCreatedBy} has just commented on this post—"${postTitle}"`);
+      }
+}
+
+const user1= new Notification("345")
+user1.send('John Doe', 'This is a great post') // Notifications John Doe has just commented on this post— "
